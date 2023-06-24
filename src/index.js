1cf3b3d6cae3abb80194ca3ec3734189ad375426
Ismail099 <hedaribo09@gmail.com>
2023-06-24 21:04:11 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import 'status-indicator/styles.css';
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
